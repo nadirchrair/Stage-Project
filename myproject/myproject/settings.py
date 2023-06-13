01f2dd54840b4d280c02d0f70a89c3de5d35bf67
nadir chrair <chrairmohamednadir@gmail.com>
2023-06-13 20:27:43 +0100

    add-sql backend

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
